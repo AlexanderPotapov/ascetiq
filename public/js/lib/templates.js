1f39b2d04f064327b131044a1875d61a4338fa74
AlexanderPotapov <me@alexander.pt>
2020-02-07 18:28:53 +0100

    no message

--- public/js/lib/templates.js
+++ public/js/lib/templates.js
@@ -3,7 +3,7 @@
 define({
 
 	help: '<pre>                                                                \n' +
-		'Cryptalk, encrypted instant chat.                                      \n' +
+		'Enignum, encrypted instant chat.                                      \n' +
 		'                                                                       \n' +
 		'----------------------------------------------------------------------	\n' +
 		'                                                                       \n' +
