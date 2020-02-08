924db71fe8310d0cd80831243b480148f0514113
AlexanderPotapov <me@alexander.pt>
2020-02-08 16:49:44 +0100

    no message

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 #!/usr/bin/env node
 
+app.use (function (req, res, next) {
+        if (req.secure) {
+                // request was via https, so do no special handling
+                next();
+        } else {
+                // request was via http, so redirect to https
+                res.redirect('https://' + req.headers.host + req.url);
+        }
+});
+
 const
 	files = require('node-static'),
 	port = process.env.PORT || 8080,
