1eaa1a951e73279a2ff775f99f845985d5959e00
AlexanderPotapov <me@alexander.pt>
2020-02-08 16:56:26 +0100

    no message

--- server.js
+++ server.js
@@ -1,5 +1,24 @@
 #!/usr/bin/env node
 
+var express = require('express'),
+   env = process.env.NODE_ENV || 'development';
+
+ var forceSsl = function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+        return res.redirect(['https://', req.get('Host'), req.url].join(''));
+    }
+    return next();
+ };
+
+ app.configure(function () {
+
+    if (env === 'production') {
+        app.use(forceSsl);
+    }
+
+    // other configurations etc for express go here...
+}
+
 const
 	files = require('node-static'),
 	port = process.env.PORT || 8080,
