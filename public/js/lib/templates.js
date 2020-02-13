f7ecb7f8fae7aeab1639eae863e893487e25b10e
AlexanderPotapov <me@alexander.pt>
2020-02-13 18:54:57 +0100

    no message

--- public/js/lib/templates.js
+++ public/js/lib/templates.js
@@ -46,11 +46,11 @@ define({
 	// along with the current nick, room, mute-status and of course the message ('text').
 	post: {
 				motd: 		'<li id="{id}" style="animation: fadeOut{id} ease 60s; -webkit-animation: fadeOut{id} ease 60s; -moz-animation: fadeOut{id} ease 60s; -o-animation: fadeOut{id} ease 60s; -ms-animation: fadeOut{id} ease 60s; 
-		| @keyframes fadeOut{id} {0% {opacity:1;}100% {opacity:0;}}
-		| @-moz-keyframes fadeOut{id} {0% {opacity:1;}100% {opacity:0;}}
-		| @-webkit-keyframes fadeOut{id} {0% {opacity:1;}100% {opacity:0;}}
-		| @-o-keyframes fadeOut{id} {0% {opacity:1;}100% {opacity:0;}}
-		| @-ms-keyframes fadeOut{id} {0% {opacity:1;}100% {opacity:0;}}
+		 @keyframes fadeOut{id} `{0% {opacity:1;}100% {opacity:0;}}`
+		 @-moz-keyframes fadeOut{id} `{0% {opacity:1;}100% {opacity:0;}}`
+		 @-webkit-keyframes fadeOut{id} `{0% {opacity:1;}100% {opacity:0;}}`
+		 @-o-keyframes fadeOut{id} `{0% {opacity:1;}100% {opacity:0;}}`
+		 @-ms-keyframes fadeOut{id} `{0% {opacity:1;}100% {opacity:0;}}`
 "><i class="motd">{text}</i></li>',
 
 
