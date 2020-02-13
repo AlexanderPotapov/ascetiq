8e3c1bc5df3ff719aeb9333896959fba0a732f5e
AlexanderPotapov <me@alexander.pt>
2020-02-13 14:55:12 +0100

    no message

--- public/js/lib/settings.js
+++ public/js/lib/settings.js
@@ -4,17 +4,7 @@ define({
 
 	ttl: 1200000,
 
-	motd: '
-		<pre>Ascetiq is the first online chat that simulate the real talk</b>.
-		<br /><br />
-		- Invite the person to the same chat to start the conversation; 
-  <br />- Only persons you have invited can read this, as all messages are encrypted in browser;
-  <br />- Messages are gone forever in 1 minute after being sent.<br /><br />
-        Ascetiq does not store messages or has access to them, as conversations 
-        are encrypted in your browser by random key or key that you have set.
-        <br /><br/>Try /help, /nick, /title, /count ...<br /><br/>
-		<hr />
-        </pre>',
+	motd: '<pre><b>Ascetiq is the first online chat that simulate the real talk</b>.</pre>',
 
 	nick: {
 		maxLen: 20,
